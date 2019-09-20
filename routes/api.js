f3ba041ae2c1e8b3ba4bb730af8ebebf8779a721
Josh Latour <jlatour88@gmail.com>
2019-09-20 14:08:48 -0500

    removed some code for testing

--- routes/api.js
+++ routes/api.js
@@ -1,36 +1,33 @@
 module.exports = function(app, passport) {
    
    // Login
-   app.get('/api/login', function(req, res) {
-      console.log('made it to /api/login');
-      res.redirect('/auth/eveonline');
-   });
+   // app.get('/api/login', function(req, res) {
+   //    console.log('made it to /api/login');
+   //    res.redirect('http:google.com');
+   // });
+
 
    //  Login proccess
    app.get('/auth/eveonline', 
-      passport.authenticate('eveonline'), 
-      function(req, res) {
-         console.log('Made it to /auth/eveonline');
-         res.status(200).json({
-            success: true
-         });
-      }
+      passport.authenticate('eveonline-sso')
    );
 
    // Callback
    app.get(
       '/auth/eveonline/callback',
-      passport.authenticate('eveonline', {
-         successRedirect: '/',
-         failureRedirect: '/error'
+      passport.authenticate('eveonline-sso', {
+         session: false
+         // successRedirect: '/dashboard',
+         // failureRedirect: '/error'
       }),
       function(req, res) {
-         var code = req.query.code;
-         var state = req.query.state;
-         console.log('Made it to callback');
-         console.log(code);
-         console.log(state);
-         // Place something here
+      //    var code = req.query.code;
+      //    var state = req.query.state;
+      //    console.log('Made it to callback');
+      //    console.log(code);
+      //    console.log(state);
+      //    // Place something here
+      res.redirect('/dashboard')
       }
    );
 
