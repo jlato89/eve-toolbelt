09ee4d1229f57bc3fee01b6b09d1f4d6c22e9d8b
Josh Latour <jlatour88@gmail.com>
2019-09-21 13:14:04 -0500

    client/src/components/Login:

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,14 +4,6 @@ import axios from 'axios';
  class Login extends Component {
 
    componentDidMount() {
-      // axios
-      //    .get('auth/eveonline')
-      //    .then(res => {
-
-      //       console.log(res.data);
-      //    })
-      //    .catch(err => console.log('Login.js AXIOS: ', err));
-      window.location.href = 'http://localhost:8080/auth/eveonline';
    }
 
    render() {
