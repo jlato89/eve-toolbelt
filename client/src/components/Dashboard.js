c2e0bc0b33bbc50ae3525f28f4a03c3cf999ee86
Josh Latour <jlatour88@gmail.com>
2019-09-21 13:12:55 -0500

    added comment

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
 export class Dashboard extends Component {
-   render() {
+   componentDidMount() {
+      // grab auth data from db
+   }
+
+   render() { 
       return (
          <div>
             <h1>Dashboard Page</h1>
