d88880fde79b7423f2c68b2e73560d58fa6e3382
Josh Latour <jlatour88@gmail.com>
2019-09-21 13:12:05 -0500

    changed login to redirect

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {Redirect} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
+import HomePage from './components/HomePage';
 import Dashboard from './components/Dashboard';
 import NoMatch from './components/NoMatch';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Router>
-          <Switch>
-            <Route exact path ='/' component={Login} />
-            <Route exact path ='/dashboard' component={Dashboard} />
-            <Route component={NoMatch} />
-          </Switch>
-        </Router>
-      </div>
+       <div>
+          <Router>
+             <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route
+                   exact path='/login' component={() => {
+                     window.location.href =
+                        'http://localhost:8080/auth/eveonline';
+                   }}
+                />
+                <Route exact path='/dashboard' component={Dashboard} />
+                <Route component={NoMatch} />
+             </Switch>
+          </Router>
+       </div>
     );
   }
 }
