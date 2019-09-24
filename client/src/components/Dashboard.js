8efda3d8e3f8d5b36c0e320b59e43685cd66151a
Josh Latour <jlatour88@gmail.com>
2019-09-24 12:27:47 -0500

    added fetch for testing /user

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 
 export class Dashboard extends Component {
+   constructor() {
+      super();
+      this.state = {
+         user: [],
+         tokens: []
+      }
+   }
    componentDidMount() {
-      // grab auth data from db
+      fetch('/user', {mode: 'cors'})
+         .then(res => {
+            console.log(res);
+            return res.json();
+         })
+         .then(user => {
+            console.log(user);
+            this.setState({ user });
+         });
    }
 
    render() { 
       return (
-         <div>
+         <>
             <h1>Dashboard Page</h1>
-         </div>
+            <h2>Welcome {this.state.user.CharacterName}!</h2>
+         </>
       );
    }
 }
