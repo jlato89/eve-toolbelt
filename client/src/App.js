15ee6737601c57db2f170218a9800bcf3bf4fd60
Josh Latour <jlatour88@gmail.com>
2019-09-24 12:29:13 -0500

    made some changes for testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,53 @@
-import React, {Component} from 'react';
-import {Redirect} from 'react-router-dom'
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './components/Login';
 import HomePage from './components/HomePage';
 import Dashboard from './components/Dashboard';
 import NoMatch from './components/NoMatch';
+import Login from './components/Login';
 
 class App extends Component {
-  render() {
-    return (
-       <div>
-          <Router>
-             <Switch>
-                <Route exact path='/' component={HomePage} />
-                <Route
-                   exact path='/login' component={() => {
-                     window.location.href =
-                        'http://localhost:8080/auth/eveonline';
-                   }}
-                />
-                <Route exact path='/dashboard' component={Dashboard} />
-                <Route component={NoMatch} />
-             </Switch>
-          </Router>
-       </div>
-    );
-  }
+   // state = [
+   //    (eveAccessToken = ''),
+   //    (eveRefreshToken = ''),
+   //    (eveCharacterId = ''), // 95271542
+   //    (eveResults = [])
+   // ];
+   componentDidMount() {
+      //    const accessToken = 'Bearer '+ {accessToken}
+      //    const charId = {characterId};
+      //    const requests = {requests}
+      //    const queryURL =
+      //       'https://esi.evetech.net/latest/characters/'+charId+'/'+requests+'/';
+      //    axios.get(
+      //       queryURL,
+      //       {
+      //          'Authorization': accessToken
+      //       }
+      //    );
+   }
+
+   render() {
+      return (
+         <div>
+            <Router>
+               <Switch>
+                  <Route exact path='/' component={HomePage} />
+                  <Route exact path='/login2' component={Login} />
+                  <Route
+                     exact
+                     path='/login'
+                     component={() => {
+                        window.location.href =
+                           'http://localhost:8080/auth/eveonline';
+                     }}
+                  />
+                  <Route exact path='/dashboard' component={Dashboard} />
+                  <Route component={NoMatch} />
+               </Switch>
+            </Router>
+         </div>
+      );
+   }
 }
 
 export default App;
