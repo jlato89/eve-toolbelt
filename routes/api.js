fd7aab1e00d99c51ff3d674289615ee1710b472e
Josh Latour <jlatour88@gmail.com>
2019-09-25 21:38:44 -0500

    commented out some logs, cleaned up code

--- routes/api.js
+++ routes/api.js
@@ -2,22 +2,12 @@ module.exports = function(app, passport) {
    
    app.get('/api/user', (req, res) => {
       if (!req.user) {
-         console.log('User is NOT logged in!');
+         // console.log('User is NOT logged in!');
          res.redirect('/')
       } else {
-         console.log('User is logged in');
+         // console.log('User is logged in');
          const user = req.user._json;
-         // const accessToken = req.authInfo.accessToken;
-         // const refreshToken = req.authInfo.refreshToken;
-         // console.log('passport user: \n',req.user._json);
-         console.log('passport tokens: \n',req.authInfo); //! not able to pass from passport
-         res.json({
-            // name: 'Josh',
-            // location: 'Nashville, Tn',
-            user
-            // accessToken,
-            // refreshToken
-         })
+         res.json(user)
       }
    })
 
@@ -31,15 +21,12 @@ module.exports = function(app, passport) {
       '/auth/eveonline/callback',
       passport.authenticate('eveonline-sso', {
          session: true,
-         // successRedirect: 'http://localhost:3000/dashboard',
          failureRedirect: 'http://localhost:3000/error'
       }),
       function(req, res) {
          console.log('~~~ Made it to callback ~~~');
-         // console.log('accessToken: \n',req.authInfo.accessToken);
-         // console.log('refreshToken: \n',req.authInfo.refreshToken);
          // console.log('profile: \n', req.user._json);
-
+         
          res.redirect('http://localhost:3000/dashboard');
       }
    );
