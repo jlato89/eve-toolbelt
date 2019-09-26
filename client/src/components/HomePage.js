858cce05d1ceaf2c2150cad7e2707a9235d3721a
Josh Latour <jlatour88@gmail.com>
2019-09-25 19:13:56 -0500

    added img for login button

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -5,7 +5,12 @@ export class HomePage extends Component {
       return (
          <>
             <h1>HomePage</h1>
-            <a href='http://localhost:8080/auth/eveonline'>Login Here</a>
+            <a href='http://localhost:8080/auth/eveonline'>
+               <img
+                  src='https://web.ccpgamescdn.com/eveonlineassets/developers/eve-sso-login-black-large.png'
+                  alt='login w/ eve button'
+               />
+            </a>
          </>
       );
    }
