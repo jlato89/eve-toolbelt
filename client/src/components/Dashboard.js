738f64a915a3a83fe83c55d30fbc5a33497a5998
Josh Latour <jlatour88@gmail.com>
2019-09-27 08:53:24 -0500

    figuring out how to grab info from api

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -8,14 +8,15 @@ export class Dashboard extends Component {
          location: {}
       }
    }
-   componentDidMount() {
-      fetch('/api/user', {mode: 'cors'})
+
+   async componentDidMount() {
+      await fetch('/api/user', {mode: 'cors'})
          .then(res => {
-            console.log('DASHBOARD: ',res);
+            // console.log('DASHBOARD: ',res);
             return res.json();
          })
          .then(user => {
-            console.log('DASHBOARD User: ',user);
+            // console.log('DASHBOARD User: ',user);
             this.setState(user);
          })
          .catch(err => {
@@ -23,6 +24,40 @@ export class Dashboard extends Component {
             alert('Please Login First!')
             window.location.href = '/';
          });
+
+      // console.log('UserId: ',this.state.user.characterName);
+      // const userId = this.state.user.characterID;
+      // const url = 'https://esi.evetech.net/latest/characters/'+userId+'/location';
+      // await fetch('https://esi.evetech.net/latest/characters/'+this.state.user.characterID+'/location', {
+      //    headers: {
+      //       Authorization: 'Bearer '+this.state.user.accessToken
+      //    }
+      //    })
+      //    .then(res => {
+      //       return res.json()
+      //    })
+      //    .then(data => {
+      //       console.log(data);
+      //       if (data.sso_status === 401) {
+      //          console.log('access Token Expired');
+      //          fetch('https://login.eveonline.com/v2/oauth/token HTTP/1.1', {
+      //             headers: {
+      //                Content-Type: 'application/x-www-form-urlencoded',
+      //                Host: 'login.eveonline.com',
+      //                Authorization: 'Basic '+this.user.refreshToken 
+      //             }
+      //          })
+      //             .then(res => {
+      //                return res.json();
+      //             })
+      //             .then(token => {
+      //                console.log('Token: ', token);
+      //             });
+      //       }
+      //    })
+      //    .catch(err => {
+      //       console.log('ESI Err: \n',err);
+      //    })
    }
 
    render() { 
