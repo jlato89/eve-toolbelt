66fcae7a1597dbec07c21abb556af1b3ba07702c
Josh Latour <jlatour88@gmail.com>
2019-09-28 10:26:42 -0500

    finished token refresh route and defined specific data to be sent to react on login(to exclude refresh token)

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,14 @@
+const request = require('request');
+
 module.exports = function(app, passport, db) {
    
+   //* Grab user Info from DB
    app.get('/api/user', (req, res) => {
       if (!req.user) {
-         // console.log('User is NOT logged in!');
+         console.log('User not logged in. Redirecting to /');
          res.redirect('/')
       } else {
-         // console.log('User is logged in');
+         // Grab user data from ID of currently logged in user
          const currentUser = req.user.CharacterID;
          db.user.findOne({
             where: {
@@ -14,18 +17,88 @@ module.exports = function(app, passport, db) {
          })
          .then((data) => {
             const user = data.dataValues
-            console.log(user);
-            res.json({user})
+            res.json({
+               user: {
+                  characterID: user.characterID,
+                  characterName: user.characterName,
+                  scopes: user.scopes,
+                  accessToken: data.accessToken
+               }
+            })
          });
       }
    });
 
-   //  Login proccess
+   //* Refresh Tokens from EVE AUTH
+   app.get('/api/token/:id', (req, res) => {
+      const characterID = req.params.id;
+      console.log('ID: ', id);
+
+      db.user
+         .findOne({
+            where: {
+               characterID: characterID
+            }
+         })
+         .then(data => {
+            const refreshToken = data.refreshToken;
+            const rawAuth =
+               process.env.EVEONLINE_CLIENT_ID +
+               ':' +
+               process.env.EVEONLINE_SECRET_KEY;
+            // Encode into base64
+            const buff = new Buffer.from(rawAuth);
+            const auth64 = buff.toString('base64');
+
+            const options = {
+               method: 'POST',
+               url: 'https://login.eveonline.com/v2/oauth/token',
+               headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded',
+                  Host: 'login.eveonline.com',
+                  Authorization: 'Basic ' + auth64
+               },
+               form: {
+                  grant_type: 'refresh_token',
+                  refresh_token: refreshToken
+               }
+            };
+
+            request(options, function(error, response, body) {
+               if (error) throw new Error(error);
+
+               // Convert body to JSON
+               const data = JSON.parse(body)
+
+               db.user
+                  .update(
+                     {
+                        accessToken: data.access_token,
+                        refreshToken: data.refresh_token
+                     },
+                     {
+                        where: {
+                           characterID: characterID
+                        }
+                     }
+                  )
+                  .then(res => {
+                     console.log('DB Records Updated: ', res);
+                  })
+                  .catch(err => {
+                     console.log('DB Error: ', err);
+                  });
+               // console.log(data);
+            });
+         });
+   })
+
+   //*  EVE Online Login proccess
    app.get('/auth/eveonline', 
       passport.authenticate('eveonline-sso')
    );
 
-   // Callback
+   //* EVE Online Callback
    app.get(
       '/auth/eveonline/callback',
       passport.authenticate('eveonline-sso', {
@@ -40,7 +113,7 @@ module.exports = function(app, passport, db) {
       }
    );
 
-   // Logout
+   //* Logout
    app.get('/logout', function(req, res) {
       req.session.destroy(function(err) {
          res.redirect('/');
