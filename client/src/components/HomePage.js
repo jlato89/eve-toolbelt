c71be62607207e6f059d621d90badaf6eadaf293
Josh Latour <jlatour88@gmail.com>
2019-09-29 22:04:01 -0500

    added islogged in statement and state placeholder

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 
 export class HomePage extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         loggedIn: false,
+         characterName: ''
+      };
+   }
+
    render() {
-      return (
-         <>
-            <h1>HomePage</h1>
-            <a href='http://localhost:8080/auth/eveonline'>
-               <img
-                  src='https://web.ccpgamescdn.com/eveonlineassets/developers/eve-sso-login-black-large.png'
-                  alt='login w/ eve button'
-               />
-            </a>
-         </>
-      );
+      if (!this.state.loggedIn) {
+         return (
+            <>
+               <h1>HomePage</h1>
+               <a href='http://localhost:8080/auth/eveonline'>
+                  <img
+                     src='https://web.ccpgamescdn.com/eveonlineassets/developers/eve-sso-login-black-large.png'
+                     alt='login w/ eve button'
+                  />
+               </a>
+            </>
+         );
+      } else {
+         return (
+            <>
+               <h1>HomePage</h1>
+               <h3>You are logged</h3>
+            </>
+         )
+      }
    }
 }
 
