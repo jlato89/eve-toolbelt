7b64d19713c81b643e6110abcda1a4bf276d7dff
Josh Latour <jlatour88@gmail.com>
2019-10-03 12:14:02 -0500

    Initial commit

---
+++ client/src/components/CharacterPortrait.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class CharacterPortrait extends Component {
+   constructor() {
+      super();
+      this.state = {
+         characterID: '',
+         CharacterPortrait: {}
+      };
+   }
+
+   UNSAFE_componentWillReceiveProps(props) {
+      // Check if characterID exists
+      if (props.characterID) {
+         axios
+            .post('/api/data', {
+               dataType: 'characters',
+               characterID: props.characterID,
+               endPoint: 'portrait'
+            })
+            .then(res => {
+               console.log(res.data);
+               this.setState({CharacterPortrait: res.data})
+            })
+            .catch(err => console.log(err));
+      } else {
+         console.log('no character id');
+      }
+   }
+
+   render() {
+      return (
+         <div>
+            <p>
+               <img src={this.state.CharacterPortrait.px128x128} alt='Character Portrait'></img>
+            </p>
+         </div>
+      );
+   }
+}
+
+export default CharacterPortrait;
