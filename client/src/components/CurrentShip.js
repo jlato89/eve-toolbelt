5f0c8434c3c5734d1800e47cd2fe9ee3d948533c
Josh Latour <jlatour88@gmail.com>
2019-10-02 22:25:16 -0500

    added CurrentShip component. WIP

---
+++ client/src/components/CurrentShip.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class CurrentShip extends Component {
+   constructor() {
+      super();
+      this.state = {
+         characterID: '',
+         currentShip: {}
+      };
+   }
+
+   UNSAFE_componentWillReceiveProps(props) {
+      // Check if characterID exists
+      if (props.characterID) {
+         axios
+            .post('/api/data', {
+               dataType: 'characters',
+               characterID: props.characterID,
+               endPoint: 'ship'
+            })
+            .then(res => {
+               console.log(res.data);
+               this.setState({currentShip: res.data})
+            })
+            .catch(err => console.log('Wallet.js AXIOS: ', err));
+      } else {
+         console.log('no character id');
+      }
+   }
+
+   render() {
+      return (
+         <div>
+            <p>
+               Current Ship - SHIP({this.state.currentShip.ship_name})
+            </p>
+         </div>
+      );
+   }
+}
+
+export default CurrentShip;
