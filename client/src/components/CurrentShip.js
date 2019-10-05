be92b0ed8a14efed2a453ba5bf00c7786424f3ff
Josh Latour <jlatour88@gmail.com>
2019-10-05 00:11:35 -0500

    adding new static data. WIP

--- client/src/components/CurrentShip.js
+++ client/src/components/CurrentShip.js
@@ -6,7 +6,8 @@ class CurrentShip extends Component {
       super();
       this.state = {
          characterID: '',
-         CurrentShip: {}
+         CurrentShip: {},
+         staticData: {}
       };
    }
 
@@ -21,7 +22,10 @@ class CurrentShip extends Component {
             })
             .then(res => {
                console.log(res.data);
-               this.setState({CurrentShip: res.data})
+               this.setState({
+                  CurrentShip: res.data.data,
+                  staticData: res.data.data2
+               })
             })
             .catch(err => console.log(err));
       } else {
@@ -33,7 +37,8 @@ class CurrentShip extends Component {
       return (
          <div>
             <p>
-               Current Ship - SHIP({this.state.CurrentShip.ship_name})
+               Current Ship - {this.state.staticData}(
+               {this.state.CurrentShip.ship_name})
             </p>
          </div>
       );
